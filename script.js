3837bc5e2882c1027eba74787d52f7ab284ef1b7
luisevb24 <luisevb.5s@gmail.com>
2025-05-01 16:17:38 -0600

    Make function to play a round, making it case insensitive

--- script.js
+++ script.js
@@ -7,38 +7,71 @@ If the number is greater than 3.33 and less or equal to 6.66 RETURN "paper"
 IF THE NUMBER IS GREATER THAN 6.66 AND LESS OR EQUAL TO 10 RETURN "SCISSORS"
 
 */
-let computerChoice = "not set"
 function getComputerChoice(){
     let randomNumber = Math.random();
     randomNumber = randomNumber * 10;
-    console.log(randomNumber);
+
     if(randomNumber <= 3.33){
-        computerChoice = "Rock";
+        return "Rock";
     } else if (randomNumber > 3.33 && randomNumber <= 6.66){
-        computerChoice = "Paper";
+        return "Paper";
     } else if (randomNumber > 6.66 && randomNumber <= 10){
-        computerChoice = "Scissors";
+        return "Scissors";
     } else {
         console.log("Error");
     }
 }
-
-getComputerChoice();
-console.log(computerChoice);
 /*
 GETHUMANCHOICE FUNCTION PROMPS FOR A USER INPUT OF THEIR CHOICE
 IF HUMAN TYPES "ROCK" RETURN "ROCK"
 ELSE IF HUMAN TYPES "PAPER" RETURN "PAPER"
 ELSE IF HUMAN TYPES "SCISSORS" RETURN "SCISSORS";
 */
-let humanChoice = "Rock";
-
 function getHumanChoice(){
-    humanChoice = prompt("Make your choice!", "Rock");
-    return humanChoice;
+    return prompt("Make your choice!", "Rock");
 }
-getHumanChoice();
-console.log(humanChoice);
-
 /*
+VARIABLES THAT WILL TRACK THE SCORES ARE INITIALIZED
 */
+
+let humanScore = 0;
+let computerScore = 0;
+
+// FUNCTION PLAYROUND TAKES AS ARGUMENTS VARIABLES HUMANCHOICE AND COMPUTERCHOICE
+// THIS FUNCTION TAKES THE HUMANCHOICE AND MAKES IT ALL LOWERCASE USING STRING METHOD
+// MAKE A CONDITIONAL SWITCH CASE FOR WHEN COMPUTERCHOICE IS ROCK;
+
+function playRound(humanChoice, computerChoice){
+    humanChoiceLower = humanChoice.toLowerCase();
+    
+    if (computerChoice == "Rock" && humanChoiceLower == "rock"){
+        console.log("Tie! Rock ties with Rock");
+    } else if (computerChoice == "Rock" && humanChoiceLower == "paper"){
+        console.log("You win! Rock loses to Paper");
+        ++humanScore;
+    } else if (computerChoice == "Rock" && humanChoiceLower == "scissors"){
+        console.log("You lose! Rock beats Scissors");
+        ++computerScore;
+    } else if (computerChoice == "Paper" && humanChoiceLower == "rock"){
+        console.log("You lose! Paper beats Rock");
+        ++computerScore;
+    } else if (computerChoice == "Paper" && humanChoiceLower == "scissors"){
+        console.log("You win! Paper loses to Scissors");
+        ++humanScore;
+    } else if (computerChoice == "Paper" && humanChoiceLower == "paper"){
+        console.log("Tie!");
+    } else if (computerChoice == "Scissors" && humanChoiceLower == "rock"){
+        console.log("You win! Scissors loses to Rock");
+        ++humanScore;
+    } else if (computerChoice == "Scissors" && humanChoiceLower == "paper"){
+        console.log("You lose! Scissors beats Paper");
+        ++computerScore;
+    } else if (computerChoice == "Scissors" && humanChoiceLower == "scissors"){
+        console.log("Tie");
+    }
+
+}
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+playRound(humanSelection, computerSelection);
+
