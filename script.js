527246a90343480979d74eb13a5c962db6a446c8
luisevb24 <luisevb.5s@gmail.com>
2025-05-09 11:34:31 -0600

    add ui

--- script.js
+++ script.js
@@ -1,11 +1,34 @@
-/*
-Pseudocode:
-A function gets a random number between 0 and 1 and multiplies it by 10.
-If the number is less than or equal to 3.33 RETURN "rock"
-If the number is greater than 3.33 and less or equal to 6.66 RETURN "paper"
-IF THE NUMBER IS GREATER THAN 6.66 AND LESS OR EQUAL TO 10 RETURN "SCISSORS"
+let body = document.querySelector("body");
+let rockBtn = document.querySelector(".rockBtn");
+let paperBtn = document.querySelector(".paperBtn");
+let scissorsBtn = document.querySelector(".scissorsBtn");
+let scoreDiv = document.querySelector(".score");
+let resultsDiv = document.querySelector(".results");
+let finalDiv = document.querySelector(".finalScreen");
+let resetBtn = document.createElement('button');
+resetBtn.textContent = "Play Again!";
 
-*/
+rockBtn.addEventListener("click", () => {
+    playRound('rock', getComputerChoice());
+});
+paperBtn.addEventListener("click", () => {
+    playRound('paper', getComputerChoice());
+});
+scissorsBtn.addEventListener("click", () => {
+    playRound('scissors', getComputerChoice());
+});
+
+resetBtn.addEventListener('click', ()=>{
+    resetScores();
+    finalDiv.textContent = "";
+})
+
+
+let humanScore = 0;
+let computerScore = 0;
+
+
+
 function getComputerChoice(){
     let randomNumber = Math.random();
     randomNumber = randomNumber * 10;
@@ -20,74 +43,51 @@ function getComputerChoice(){
         console.log("Error");
     }
 }
-/*
-GETHUMANCHOICE FUNCTION PROMPS FOR A USER INPUT OF THEIR CHOICE
-IF HUMAN TYPES "ROCK" RETURN "ROCK"
-ELSE IF HUMAN TYPES "PAPER" RETURN "PAPER"
-ELSE IF HUMAN TYPES "SCISSORS" RETURN "SCISSORS";
-*/
-function getHumanChoice(){
-    return prompt("Make your choice!", "Rock");
-}
-//PLAYGAME FUNCTION CALLS PLAYROUND FUNCTION 5 TIMES, AND DECLARES A WINNER BASED ON THE SCORES VARIABLES
-
-function playGame(){
-/*
-VARIABLES THAT WILL TRACK THE SCORES ARE INITIALIZED
-*/
-
-let humanScore = 0;
-let computerScore = 0;
-
-// FUNCTION PLAYROUND TAKES AS ARGUMENTS VARIABLES HUMANCHOICE AND COMPUTERCHOICE
-// THIS FUNCTION TAKES THE HUMANCHOICE AND MAKES IT ALL LOWERCASE USING STRING METHOD
-// MAKE A CONDITIONAL SWITCH CASE FOR WHEN COMPUTERCHOICE IS ROCK;
 
 function playRound(humanChoice, computerChoice){
     humanChoiceLower = humanChoice.toLowerCase();
     
+    
     if (computerChoice == "Rock" && humanChoiceLower == "rock"){
-        console.log("Tie!");
+        resultsDiv.textContent = "Tie!";
     } else if (computerChoice == "Rock" && humanChoiceLower == "paper"){
-        console.log("Point for you! Rock loses to Paper");
+        resultsDiv.textContent = "Point for you! Rock loses to Paper";
         ++humanScore;
     } else if (computerChoice == "Rock" && humanChoiceLower == "scissors"){
-        console.log("Point for the PC! Rock beats Scissors");
+        resultsDiv.textContent = "Point for the PC! Rock beats Scissors";
         ++computerScore;
     } else if (computerChoice == "Paper" && humanChoiceLower == "rock"){
-        console.log("Point for the PC! Paper beats Rock");
+        resultsDiv.textContent = "Point for the PC! Paper beats Rock";
         ++computerScore;
     } else if (computerChoice == "Paper" && humanChoiceLower == "scissors"){
-        console.log("Point for you! Paper loses to Scissors");
+        resultsDiv.textContent = "Point for you! Paper loses to Scissors";
         ++humanScore;
     } else if (computerChoice == "Paper" && humanChoiceLower == "paper"){
-        console.log("Tie!");
+        resultsDiv.textContent = "Tie!";
     } else if (computerChoice == "Scissors" && humanChoiceLower == "rock"){
-        console.log("Point for you! Scissors loses to Rock");
+        resultsDiv.textContent = "Point for you! Scissors loses to Rock";
         ++humanScore;
     } else if (computerChoice == "Scissors" && humanChoiceLower == "paper"){
-        console.log("Point for the PC! Scissors beats Paper");
+        resultsDiv.textContent = "Point for the PC! Scissors beats Paper";
         ++computerScore;
     } else if (computerChoice == "Scissors" && humanChoiceLower == "scissors"){
-        console.log("Tie");
+        resultsDiv.textContent = "Tie" ;
+    }
+
+    scoreDiv.textContent = `You: ${humanScore} PC: ${computerScore}`;
+
+    if (humanScore >= 5){
+        finalDiv.textContent = "You Win!";
+        body.appendChild(resetBtn);
+    }
+    if (computerScore >= 5){
+        finalDiv.textContent = "You Lose :("
+        body.appendChild(resetBtn);
     }
 }
 
-for (let roundsPlayed = 0; roundsPlayed < 5; roundsPlayed++) {
-        playRound(getHumanChoice(), getComputerChoice());
-        console.log("Your Score: " + humanScore +
-                    " Computer's Score: " + computerScore
-        )
-  }
-if(humanScore > computerScore){
-    console.log("You Win!");
-} else if (humanScore < computerScore){
-    console.log("You Lose :(");
-} else if (humanScore == computerScore){
-    console.log("Is a tie");
-} else{
-    console.log("theres something weird going on");
+function resetScores() {
+    humanScore = 0;
+    computerScore = 0;
+    scoreDiv.textContent = `You: ${humanScore} PC: ${computerScore}`;
 }
-}
-
-playGame();
